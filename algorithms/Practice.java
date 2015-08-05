e163e4fbf3a845f08fc89c4f4b6c79a02d6d2e38
gummyegg <gummyegg@gummyegg0.mtv.corp.google.com>
2015-08-05 08:59:37 -0700

    Prime push.

--- algorithms/Practice.java
+++ algorithms/Practice.java
@@ -88,4 +88,16 @@ public class Practice {
 		circle.next = treeToCircle(tree.right);
 		return circle;
 	}
+	
+	public boolean isPrime(int n) {
+	  if (n % 2 == 0) {
+	    return false;
+	  }
+	  for (int i = 3; i * i <= n; i += 2) {
+	    if (n % i == 0) {
+	      return false
+	    }
+	  }
+	  return true;
+	}
 }
