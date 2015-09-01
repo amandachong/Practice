e80c347500e58cdde5faff2e8f1ce8a25fe59350
amandachong <amanda.chong@live.ca>
2015-09-01 12:24:18 -0700

    More Uber questions.

---
+++ companies/UberTreeSerializer.java
@@ -0,0 +1,13 @@
+/**
+ * Serialize & deserialize a binary tree.
+ */
+public class UberTreeSerializer {
+
+	public String serialize(TreeNode root) {
+		return null;
+	}
+
+	public TreeNode deserialize(String s) {
+		return null;
+	}
+}
