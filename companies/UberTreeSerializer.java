021910d27df027391d9dc33bc198909ed535cfa8
amandachong <amanda.chong@live.ca>
2015-09-01 12:41:21 -0700

    Uber tree serializer.

--- companies/UberTreeSerializer.java
+++ companies/UberTreeSerializer.java
@@ -3,11 +3,28 @@
  */
 public class UberTreeSerializer {
 
+	private final String NULL_TOKEN = "#";
+
 	public String serialize(TreeNode root) {
-		return null;
+		if (root == null) {
+			return NULL_TOKEN;
+		}
+		String s = "" + root.value;
+		s += serialize(root.left);
+		return s + serialize(root.right);
 	}
 
 	public TreeNode deserialize(String s) {
-		return null;
+		if (s == null) {
+			return null;
+		}
+		String value = s.substring(0, 1);
+		if (value.equals(NULL_TOKEN)) {
+			return null;
+		}
+		TreeNode head = new TreeNode(Integer.parseInt(value));
+		head.left = deserialize(s.substring(1));
+		head.right = deserialize(s.substring(2));
+		return head;
 	}
 }
